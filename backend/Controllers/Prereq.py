bb495c73c6d813bc8ee1d128c72bbcc89d2b3522
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-01 21:27:15 -0300

    ajuste

--- backend/Controllers/Prereq.py
+++ backend/Controllers/Prereq.py
@@ -6,5 +6,5 @@ from Models.Prereq.RespostaListar import RespostaListar
 class Prereq(Controller):
 
 	def Listar(self,pedido_listar):
-		id_disciplina = BDDisciplina().pegarDisciplina("WHERE nome = %s",(pedido_listar.getDisciplina())).getId()
-		return RespostaListar(Ass_disc_pre().pegarResumoAss("disciplina.id = %s",(id_disciplina)))
+		id_disciplina = BDDisciplina().pegarDisciplina("WHERE nome = %s",(pedido_listar.getDisciplina()))
+		return RespostaListar(Ass_disc_pre().pegarResumoAss("disciplina.id = %s",(id_disciplina.getId())))

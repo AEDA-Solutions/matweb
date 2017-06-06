35902c484e150a46ff3353835858978d6a54e9a1
root <root@localhost.localdomain>
2017-06-06 16:55:59 -0400

    teste mudanca sintaxe

--- frontend/app/Departamento/departamento.services.js
+++ frontend/app/Departamento/departamento.services.js
@@ -1,9 +1,13 @@
 angular.
-  module('Departamento').
-  factory('ApiDepartamento', ['$resource',
-    function($resource) {
-      return $resource('/api/Departamento/Listar', {}, {
-        Listar: { method: 'POST' },
-      });
-    }
-  ]);
+   module('Departamento').
+   factory('ApiDepartamento', ['$resource',
+      function($resource) {
+	  var methodListar = function($resource){
+		departamentos = $resource('/api/Departamento/Listar', {},
+		{Listar: {method: 'POST'},
+		});
+	  };
+	  return {
+		Listar: methodListar	
+	  };
+}]);

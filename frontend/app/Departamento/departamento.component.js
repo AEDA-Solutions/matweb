0874dc33a71a82de3c46065f63d700717a3e37b8
root <root@localhost.localdomain>
2017-06-07 07:27:43 -0400

    listar departamentos por campus

--- frontend/app/Departamento/departamento.component.js
+++ frontend/app/Departamento/departamento.component.js
@@ -28,8 +28,13 @@ angular.
         {
             ApiDepartamentoPCampus.Listar(this.formulario,function(resultado) {
                 ctrl.departamentos = resultado.corpo;
-                console.log(MatWebGlobals);
-                console.log(ctrl.departamentos);
+                for(var i = 0; campi = null; i < MatWebGlobals.campus.length; i++) {
+                    if (MatWebGlobals.campus[i].id = $routeParams.Id_campus) {
+                        campi = MatWebGlobals.campus[i];
+                        break;
+                    }
+                };
+                console.log(campi);
             }, function(error){
                 ctrl.error = error.data.mensagem;
             });

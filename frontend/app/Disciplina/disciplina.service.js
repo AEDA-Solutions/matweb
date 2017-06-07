ec553b5cdd3a3f38bd81c2d3c01a7264c82c090f
FelipeAires <felipeaires2008@gmail.com>
2017-06-07 01:15:14 -0300

    Arrumando a pasta disciplina

--- frontend/app/Disciplina/disciplina.service.js
+++ frontend/app/Disciplina/disciplina.service.js
@@ -4,7 +4,13 @@ angular.
     function($resource) {
       return $resource('/api/Disciplina/:operacao', {}, {
         Cadastrar: { method: 'POST', params: {'operacao': "Cadastrar"}}
-        Listar: {method: 'POST', params: {'operacao': "Listar"}}
+      });
+    }
+  ]).
+  factory('ApiDisciplina', ['$resource',
+    function($resource) {
+      return $resource('/api/Disciplina/:operacao', {}, {
+        Listar: { method: 'POST', params: {'operacao': "Listar"}}
       });
     }
   ]);

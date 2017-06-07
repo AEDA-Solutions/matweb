ec6be42d9f2273ecaf5d35540de303e1614caa16
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-06 21:12:17 -0300

    creada novo servico

--- frontend/app/Departamento/departamento.services.js
+++ frontend/app/Departamento/departamento.services.js
@@ -6,4 +6,11 @@ angular.
         Listar: { method: 'POST' },
       });
     }
+  ]).
+  factory('ApiDepartamentoPCampus', ['$resource',
+    function($resource) {
+        return $resource('/api/Departamento/Listar:idCampus', {}, {
+          Listar: { method: 'POST'},
+        });
+    }                                                                      
   ]);

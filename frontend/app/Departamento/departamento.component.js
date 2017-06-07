26ea49f06bf3abf69b0b5273b7bd131c9bd83f56
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-06 23:23:11 -0300

    teste lista departamento p campus

--- frontend/app/Departamento/departamento.component.js
+++ frontend/app/Departamento/departamento.component.js
@@ -21,7 +21,7 @@ angular.
   component('listarDepartpcampus', {
     templateUrl: '/app/Departamento/departPcampus.template.html',
     controller: ['ApiDepartamentoPCampus', 'MatWebGlobals', '$routeParams', function Listar(ApiDepartamentoPCampus,MatWebGlobals,$routeParams) {
-        this.formulario = {id_campus: "$routeParams.Id_campus".slice(1) , 'nome': '', 'pagina': 0, 'quantidade': 1000 };
+        this.formulario = {id_campus: $routeParams.Id_campus , 'nome': '', 'pagina': 0, 'quantidade': 1000 };
         console.log($routeParams.Id_campus);
         var ctrl = this;
         this.listar = function()

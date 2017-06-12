cfbc4fc669cbccc04a75cf1e930215dd9e12bedb
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-12 16:46:17 -0300

    detalhar oferta

--- backend/Models/Turma/Turma.py
+++ backend/Models/Turma/Turma.py
@@ -2,6 +2,7 @@ from Models.Horario.Horario import Horario as ModelHorario
 
 class Turma(object):
 	def __init__(self, turma, horarios):
+		self.horarios = []
 		self.id = turma.getId()
 		self.letra = turma.getLetra()
 		self.id_disciplina = turma.getId_disciplina()
@@ -11,6 +12,5 @@ class Turma(object):
 		self.id_professor = turma.getId_professor()
 		self.professor = turma.getProfessor()
 		self.turno = turma.getTurno()
-      self.horarios = []
       for horario in horarios:
       	self.horarios.append(ModelHorario(horario))

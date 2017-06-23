3df2dfd56a366a26b484e39d759cf308bbef75b7
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-23 17:35:54 -0300

    tentando realizar ajuste do login

--- frontend/app/Usuario/entrar.component.js
+++ frontend/app/Usuario/entrar.component.js
@@ -1,7 +1,7 @@
 angular.
   module('Usuario').
   component('usuarioEntrar', {
-    templateUrl: '/app/Usuario/index.html',
+    templateUrl: '/app/Usuario/admin.html',
     controller: ['ApiUsuario','$http','$location', 'MatWebGlobals', '$scope',function Entrar(ApiUsuario,$http,$location,MatWebGlobals,$scope) {
       this.formulario = {'usuario':'','senha':''};
       this.entrar = function()

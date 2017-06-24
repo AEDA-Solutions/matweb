38f74db8b5a7dae03299b82124ba6475f7aafc1b
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-24 16:38:36 -0300

    gerenciar campus

--- frontend/app/Campus/campus.component.js
+++ frontend/app/Campus/campus.component.js
@@ -14,4 +14,18 @@ angular.
       console.log(ctrl.erro)
       } );
     }]
+  }).
+  component('gerenciarCampus', {
+    templateUrl: '/app/Campus/campus.adm.template.html',
+    controller: ['ApiCampus', 'MatWebGlobals', '$scope', function Gerenciar(ApiCampus,MatWebGlobals,$scope) {
+  var ctrl = this;
+  ctrl.campus = [];
+        ApiCampus.Listar({ nome: "", pagina: 0, quantidade: 1000 },function(resultado) {
+            ctrl.campus = resultado.corpo;
+            MatWebGlobals.campus = resultado.corpo;
+    }, function(erro){
+        ctrl.erro = erro.data.mensagem
+      console.log(ctrl.erro)
+      } );
+    }]
   });

faf102c98f7fcca8f6944a3eb93e25a20d9ce830
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-24 16:54:39 -0300

    gerenciar campus

--- frontend/app/Campus/campus.component.js
+++ frontend/app/Campus/campus.component.js
@@ -18,14 +18,16 @@ angular.
   component('gerenciarCampus', {
     templateUrl: '/app/Campus/campus.adm.template.html',
     controller: ['ApiCampus', 'MatWebGlobals', '$scope', function Gerenciar(ApiCampus,MatWebGlobals,$scope) {
-  var ctrl = this;
-  ctrl.campus = [];
-        ApiCampus.Listar({ nome: "", pagina: 0, quantidade: 1000 },function(resultado) {
-            ctrl.campus = resultado.corpo;
-            MatWebGlobals.campus = resultado.corpo;
-    }, function(erro){
-        ctrl.erro = erro.data.mensagem
-      console.log(ctrl.erro)
-      } );
+        var ctrl = this;
+        ctrl.campus = [];
+        $scope.Listar = function() {
+            ApiCampus.Listar({ nome: "", pagina: 0, quantidade: 1000 },function(resultado) {
+                ctrl.campus = resultado.corpo;
+                MatWebGlobals.campus = resultado.corpo;
+            }, function(erro){
+                ctrl.erro = erro.data.mensagem
+                console.log(ctrl.erro)
+            } );    
+        }
     }]
-  });
+});

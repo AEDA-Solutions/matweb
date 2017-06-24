958118448818e3c1a80e22ad9b604831d94baebc
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-24 11:35:08 -0300

    Ajuste menus tela principal

--- frontend/app/nucleo/menu-principal.component.js
+++ frontend/app/nucleo/menu-principal.component.js
@@ -2,11 +2,13 @@ angular.
   module('nucleo').
   component('menuPrincipal', {
     templateUrl: '/app/nucleo/menu-principal.template.html',
-    controller: ['MatWebGlobals', function Menu(MatWebGlobals) {
-      if (typeof MatWebGlobals.usuarioLogado !== 'undefined') {
-        this.usuario = MatWebGlobals.usuarioLogado.usuario;
-      }else{
-        this.usuario = ""
+    controller: ['MatWebGlobals', '$scope', function Menu(MatWebGlobals, $scope) {
+      if ( window.sessionStorage.hasOwnProperty('UserName')) {
+          $scope.NomeUsuario = window.sessionStorage.getItem('UserName');
+          $scope.CPFUsuario = window.sessionStorage.getItem('UserCPF');
+          $scope.perfilUsuario = window.sessionStorage.getItem('UserProfile');
+      } else {
+          $scope.pefilUsuario = "";
       }
     }]
   });

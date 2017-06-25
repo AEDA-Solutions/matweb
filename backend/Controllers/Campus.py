b090b4e2bf2ac52cb5238e42fc02819f1ebe4683
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-24 22:36:38 -0300

    gerenciar campus

--- backend/Controllers/Campus.py
+++ backend/Controllers/Campus.py
@@ -22,13 +22,13 @@ class Campus(Controller):
 		return RespostaCadastrar(BDCampus().inserirCampus(campus))
 
 	def Editar(self,pedido_editar):
-		campus = BDCampus().pegarCampus("WHERE id = %s ", (pedido_editar.getId()))
+		campus = BDCampus().pegarCampus("WHERE id = %s ", (pedido_editar.getId()[0],))
 		campus.setNome(pedido_editar.getNome())
 		BDCampus().alterarCampus(campus)
 		return RespostaEditar("Campus Editado com sucesso!")
 
 	def Deletar(self,pedido_deletar):
-		campus = BDCampus().pegarCampus("WHERE id = %s ", (pedido_deletar.getId()))		
+		campus = BDCampus().pegarCampus("WHERE id = %s ", (pedido_deletar.getId()[0],))		
 		BDCampus().removerCampus(campus)
 		return RespostaDeletar("Campus Removido com sucesso!")
 

66b305c89d4f06f16dfb7fabbcc0d07a906a3d3d
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-25 08:11:58 -0300

    gerenciar usuarios

--- frontend/app/Usuario/usuario.service.js
+++ frontend/app/Usuario/usuario.service.js
@@ -6,4 +6,12 @@ angular.
         Entrar: { method: 'POST' },
       });
     }
+]). 
+module('Usuario').
+  factory('ApiUsuarioListar', ['$resource',
+    function($resource) {
+      return $resource('/api/Usuario/Listar', {}, {
+        Listar: { method: 'POST' },
+      });
+    }
 ]);

e8da0b0569d08b1b8b3d4a8cf10063c3b1dc4ef2
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-25 08:43:51 -0300

    gerenciar usuarios

--- frontend/app/Usuario/usuario.component.js
+++ frontend/app/Usuario/usuario.component.js
@@ -2,6 +2,36 @@ angular.
   module('Usuario').
   component('gerenciarUsuario', {
     templateUrl: '/app/Usuario/usuario.adm.template.html',
-    controller: ['ApiUsuario','$http','$location', 'MatWebGlobals', '$scope',function Entrar(ApiUsuario,$http,$location,MatWebGlobals,$scope) {
+    controller: ['ApiUsuarioListar','$http','$location', 'MatWebGlobals', '$scope',function Entrar(ApiUsuarioListar,$http,$location,MatWebGlobals,$scope) {
+        var ctrl = this;
+        ctrl.usuarios = [];
+        this.formulario = { 'usuario': '' };
+        
+        $scope.opcaoListar = function(){
+            $scope.opcaolistar = true;
+            ctrl.logListar = '';
+        };
+        
+        $scope.Listar = function(){
+            $scope.listando = true;
+            $scope.opcaolistar = false;
+            $scope.editando = false;
+            $scope.gravando = false;
+            $scope.selecionado = false;
+            ctrl.logListar = '';
+            if ( ctrl.formulario.usuario = '' || typeof ctrl.formulario.usuario === 'undefined' ) {
+                $scope.logListar = 'Critério de Pesquisa não Preenchido';
+            } else {
+            ApiUsuarioListar.Listar({ usuario: ctrl.formulario.usuario},function(resultado) {
+                ctrl.usuarios = resultado.corpo;
+            }, function(erro) {
+                ctrl.logListar = erro.data.mensagem;
+            });
+            };
+        };
     }]
 });
+
+
+
+ 

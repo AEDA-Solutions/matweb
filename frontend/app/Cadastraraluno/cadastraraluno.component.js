e7ed182af7726b2721d0d82c2c73464ac80fc850
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-25 18:13:15 -0300

    gerenciar usuarios

--- frontend/app/Cadastraraluno/cadastraraluno.component.js
+++ frontend/app/Cadastraraluno/cadastraraluno.component.js
@@ -23,12 +23,11 @@ angular.
       ctrl.usuario = MatWebGlobals.editUser;
       
       ctrl.setformulario = function(usuario) {
-          ctrl.fomulario = {'nome': usuario.nome, 'matricula': usuario.matricula, 'cpf': usuario.cpf, 'perfil': usuario.perfil, 'email': usuario.email, 'sexo': usuario.sexo, 'nome_pai': usuario.nome_pai, 'nome_mae': usuario.nome_mae, 'ano_conclusao': usuario.ano_conclusao, 'identidade':usuario.identidade, 'senha': usuario.senha, 'id': usuario.id};
+          ctrl.formulario = {'nome': usuario.nome, 'matricula': usuario.matricula, 'cpf': usuario.cpf, 'perfil': usuario.perfil, 'email': usuario.email, 'sexo': usuario.sexo, 'nome_pai': usuario.nome_pai, 'nome_mae': usuario.nome_mae, 'ano_conclusao': usuario.ano_conclusao, 'identidade':usuario.identidade, 'senha': usuario.senha, 'id': usuario.id};
       };
         
       $scope.Alterar = function() {
           ctrl.setformulario(ctrl.usuario);
-          console.log(ctrl.formulario);
           ApiUsuarioEditar.Editar(ctrl.formulario, function(resultado) {
               $scope.logEditar = resultado.corpo.mensagem;
           }, function(erro) {

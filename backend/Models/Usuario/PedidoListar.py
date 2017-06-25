91e74f44b28ad22534ef2a3428527398ada41f93
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-25 07:50:05 -0300

    gerenciar usuarios

---
+++ backend/Models/Usuario/PedidoListar.py
@@ -0,0 +1,14 @@
+from Framework.Pedido import Pedido
+from Framework.ErroNoHTTP import ErroNoHTTP
+
+class PedidoEntrar(Pedido):
+
+	def __init__(self,variaveis_do_ambiente):
+		super(PedidoEntrar, self).__init__(variaveis_do_ambiente)
+		try:
+			self.usuario = self.corpo['usuario']
+		except:
+			raise ErroNoHTTP(400)
+
+	def getUsuario(self):
+		return self.usuario

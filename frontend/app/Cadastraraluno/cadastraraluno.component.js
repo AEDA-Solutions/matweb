43d262879afc9533be2d8b7d84e2070cd7a7691f
Andrey Dutra Prado <andrey@localhost.localdomain>
2017-06-25 11:27:32 -0300

    gerenciar usuarios

--- frontend/app/Cadastraraluno/cadastraraluno.component.js
+++ frontend/app/Cadastraraluno/cadastraraluno.component.js
@@ -4,6 +4,7 @@ angular.
     templateUrl: '/app/Cadastraraluno/cadastraraluno.template.html',
     controller: ['ApiUsuarioCadastrar','$http','$location', 'MatWebGlobals',function(ApiUsuarioCadastrar,$http,$location,MatWebGlobals) {
       var ctrl = this;
+      console.log('cheguei aqui');
       this.formulario = {'nome':'','matricula':'','cpf':'','perfil':'','email':'','sexo':'','nome_pai':'','nome_mae':'','ano_conclusao':'','identidade':'','senha':''};
       this.cadastrar = function()
       {

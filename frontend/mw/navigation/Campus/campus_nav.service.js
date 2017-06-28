7f42ff7b8e0de533e6f75425dcfec3e4ff148474
FelipeAires <felipeaires2008@gmail.com>
2017-06-27 22:44:30 -0300

    Services - implementando navegação

---
+++ frontend/mw/navigation/Campus/campus_nav.service.js
@@ -0,0 +1,9 @@
+angular.
+  module('CampusNav').
+  factory('ApiCampusNav', ['$resource',
+    function($resource) {
+      return $resource('/api/Campus/Listar', {}, {
+        Listar: { method: 'POST' },
+      });
+    }
+  ])

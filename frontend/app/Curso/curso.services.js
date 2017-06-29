eafaf93c313d7688b4cfe6a69cb960ec3d4629a9
andreydutra <andreydutra@gmail.com>
2017-06-29 08:10:27 -0300

    gerenciar cursos

--- frontend/app/Curso/curso.services.js
+++ frontend/app/Curso/curso.services.js
@@ -42,4 +42,11 @@ factory('ApiCursoDeletar', ['$resource',
           Deletar: { method: 'POST'},
         });
     }                                                                      
+  ]). 
+factory('ApiCursoDetalhar', ['$resource',
+    function($resource) {
+        return $resource('/api/Curso/Detalhar', {}, {
+          Deletar: { method: 'POST'},
+        });
+    }                                                                      
   ]);

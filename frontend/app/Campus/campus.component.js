8a162bba702f5623edf58723b926ae6dc77d23f9
andreydutra <andreydutra@gmail.com>
2017-06-28 23:00:07 -0300

    gerenciar curso

--- frontend/app/Campus/campus.component.js
+++ frontend/app/Campus/campus.component.js
@@ -2,10 +2,11 @@ angular.
   module('Campus').
   component('listarCampus', {
     templateUrl: '/app/Campus/campus.template.html',
-    controller: ['ApiCampus', 'MatWebGlobals',function Entrar(ApiCampus,MatWebGlobals) {
-      this.nome_campus = "";
-  var ctrl = this;
-  ctrl.campus = [];
+    controller: ['ApiCampus', 'MatWebGlobals','$scope',function Entrar(ApiCampus,MatWebGlobals,$scope) {
+        this.nome_campus = "";
+        var ctrl = this;
+        ctrl.campus = [];
+        $scope.listcurso = false;
         ApiCampus.Listar({ nome: "", pagina: 0, quantidade: 1000 },function(resultado) {
             ctrl.campus = resultado.corpo;
             MatWebGlobals.campus = resultado.corpo;

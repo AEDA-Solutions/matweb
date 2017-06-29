f0dee652774ab59f85e1fc47d98201391456d8a4
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-06-29 14:58:34 -0300

    lol 2017-06-29

--- frontend/app/DadosClient/dados.component.js
+++ frontend/app/DadosClient/dados.component.js
@@ -4,10 +4,10 @@ angular.
     templateUrl: '/app/DadosClient/dados.template.html',
     controller: ['ApiDados', 'MatWebGlobals', '$scope', '$location', function Entrar(ApiDados,MatWebGlobals,$scope,$location) {
         if ( window.sessionStorage.hasOwnProperty('token_de_acesso') && window.sessionStorage.getItem('token_de_acesso') != '') {
-            $scope.nomeUsuario = MatWebGlobals.usuarioLogado.nome;
-            $scope.cpfUsuario = MatWebGlobals.usuarioLogado.cpf;
-            $scope.perfilUsuario = MatWebGlobals.usuarioLogado.perfil;
-            $scope.matriculaUsuario = MatWebGlobals.usuarioLogado.matricula;
+            $scope.nomeUsuario = MatWebGlobals.token_de_acesso.nome;
+            $scope.cpfUsuario = MatWebGlobals.token_de_acesso.cpf;
+            $scope.perfilUsuario = MatWebGlobals.token_de_acesso.perfil;
+            $scope.matriculaUsuario = MatWebGlobals.token_de_acesso.matricula;
             console.log($scope.perfilUsuario);
         } else {
             $location.path('/');

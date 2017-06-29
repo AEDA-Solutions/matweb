b2667fc48961a7ff3410fe483fbc6e78368e122a
andreydutra <andreydutra@gmail.com>
2017-06-29 18:28:56 -0300

    listar fluxos

--- frontend/app/Curso/curso.component.js
+++ frontend/app/Curso/curso.component.js
@@ -82,6 +82,7 @@ angular.
         };
         
         ctrl.FluxoListar = function(id_periodo) {
+            console.log(id_periodo);
             ApiFluxoListar.Listar({ id_periodo: id_periodo, pagina: 0, quantidade: 1000 },function(resultado) {
                 ctrl.fluxo = resultado.corpo;
             }, function(erro){

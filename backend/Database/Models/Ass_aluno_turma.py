4ba63c6cba638766753da2e51513d89e5c64ae73
henrique1998 <henri.rnp@gmail.com>
2017-06-29 20:27:08 -0300

    Create Ass_aluno_turma.py

---
+++ backend/Database/Models/Ass_aluno_turma.py
@@ -0,0 +1,33 @@
+from Database.Controllers.Turma import Turma
+from Database.Controllers.Usuario import Usuario
+from Database.Controllers.Disciplina import Disciplina
+
+class Ass_turma_prof(object):
+
+	def __init__(self,dados=None):
+		if dados is not None:
+			self.id = dados ['id']
+			self.id_turma = dados ['id_turma']
+			self.id_aluno = dados ['id_aluno']
+			
+	def getId(self):
+		return self.id
+
+	def setId_turma(self,turma,disciplina):
+		id_disciplina= (Disciplina().pegarDisciplina('nome = %s',(disciplina,))).getId()
+		self.id_turma = (Turma().pegarTurma('letra = %s AND id_disciplina = %s',(turma,id_disciplina))).getId()
+
+	def getId_turma(self):
+		return self.id_turma
+		
+	def getTurma(self):
+		return (Turma().pegarTurma('id = %s',(self.id_turma,))).getLetra()
+
+	def setId_prof(self,aluno):
+		self.id_prof = (Usuario().pegarUsuario('nome = %s',(aluno,))).getId()
+		
+	def getId_aluno(self):
+		return self.id_aluno
+	
+	def getUsuario(self):
+		return (Usuario().pegarUsuario('id = %s',(self.id_aluno,))).getNome()

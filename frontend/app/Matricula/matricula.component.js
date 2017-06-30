9fd780f2ce4eecdd18dfcc533e8e127f193949f3
andreydutra <andreydutra@gmail.com>
2017-06-30 00:02:03 -0300

    matricular

--- frontend/app/Matricula/matricula.component.js
+++ frontend/app/Matricula/matricula.component.js
@@ -2,10 +2,19 @@ angular.
   module('Matricular').
   component('usuarioMatricular', {
     templateUrl: '/app/Campus/campus.template.html',
-    controller: ['ApiMatricular','$http','$location','MatWebGlobals','$scope',function(ApiMatricular,$http,$location,MatWebGlobals,$scope) {
+    controller: ['ApiMatricular','ApiCampus', '$http','$location','MatWebGlobals','$scope',function(ApiMatricular,ApiCampus,$http,$location,MatWebGlobals,$scope) {
       var ctrl = this;
       $scope.pedindomatricula = true;
+      ctrl.campus = [];
       this.formulario = {'id_usuario':'','id_disciplina':'','id_turma':'','status':''};
+      ApiCampus.Listar({ nome: "", pagina: 0, quantidade: 1000 },function(resultado) {
+            ctrl.campus = resultado.corpo;
+            MatWebGlobals.campus = resultado.corpo;
+    }, function(erro){
+        ctrl.erro = erro.data.mensagem
+      console.log(ctrl.erro)
+      } );
+        
       this.cadastrar = function()
       {
           ApiMatricular.Cadastrar(ctrl.formulario,function(data){

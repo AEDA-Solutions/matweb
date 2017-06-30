80a6f73c1558a419d83d433c5ffd3de327391d68
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-06-30 02:30:10 -0300

    lol 2017-06-30

--- frontend/app/Matricula/matricula.services.js
+++ frontend/app/Matricula/matricula.services.js
@@ -1,9 +1,43 @@
 angular.
-  module('Matricular').
-  factory('ApiMatricular', ['$resource',
+  module('Oferta').
+  factory('ApiOferta', ['$resource',
     function($resource) {
-      return $resource('/api/Matricula/Cadastrar', {}, {
-        Cadastrar: { method: 'POST' },
+      return $resource('/api/Disciplina/Listar', {}, {
+        Listar: { method: 'POST' },
       });
     }
-  ]);
+  ]).
+  factory('ApiOfertaPDepart', ['$resource',
+    function($resource) {
+        return $resource('/api/Disciplina/Listar', {}, {
+          Listar: { method: 'POST'},
+        });
+    }                                                                      
+  ]).
+  factory('ApiOfertaDetalhar',['$resource',
+    function($resource) {
+        return $resource('/api/Oferta/Detalhar',{}, {
+            Detalhar: { method: 'POST'},
+        });
+    }                                
+]). 
+    factory('ApiDisciplinaCadastrar',['$resource',
+    function($resource) {
+        return $resource('/api/Disciplina/Cadastrar',{}, {
+            Cadastrar: { method: 'POST'},
+        });
+    }                                
+]). 
+    factory('ApiDisciplinaEditar',['$resource',
+    function($resource) {
+        return $resource('/api/Disciplina/Editar',{}, {
+            Editar: { method: 'POST'},
+        });
+    }                                
+]). factory('ApiDisciplinaDeletar',['$resource',
+    function($resource) {
+        return $resource('/api/Disciplina/Deletar',{}, {
+            Deletar: { method: 'POST'},
+        });
+    }                                
+]);

73d34935d26381376c1d37deef9020368ae5009f
andreydutra <andreydutra@gmail.com>
2017-06-30 08:30:11 -0300

    atesaet

--- frontend/app/Matricula/matricula.component.js
+++ frontend/app/Matricula/matricula.component.js
@@ -120,7 +120,7 @@ angular.
         };
         
         $scope.PedirMatricula = function(){
-            ctrl.formulario = { id_disciplina: ctrl.disciplina.id, id_usuario: window.sessionStorage.getItem('UserId'), id_turma: ctrl.turmaescolhida.id, status: "P"  };
+            ctrl.formulario = { id_disciplina: ctrl.disciplina.id, id_usuario: window.sessionStorage.getItem('UserId'), id_turma: ctrl.turmaescolhida.id, status: 'P'  };
             ApiPedirMatricula.Cadastrar(ctrl.formulario,function(resultado){
                 $scole.logPedido = "Pedido de Matricula Realizado com Sucesso";
                 ctrl.disciplina = [];

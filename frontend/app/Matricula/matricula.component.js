f1d170ae596d4163877a9dd5e5aed0506d20316e
Pedro Gabriel <pedro.gabriel.nunes@gmail.com>
2017-06-30 02:21:13 -0300

    lol 2017-06-30

--- frontend/app/Matricula/matricula.component.js
+++ frontend/app/Matricula/matricula.component.js
@@ -1,141 +1,18 @@
 angular.
   module('Matricular').
- component('ofertaDetalhar', {
+  component('usuarioMatricular', {
     templateUrl: '/app/Matricula/matricula.template.html',
-    controller: ['ApiOfertaDetalhar', 'MatWebGlobals', '$routeParams','$scope', function Detalhar(ApiOfertaDetalhar,MatWebGlobals,$routeParams,$scope) {
-        this.formulario = {'id_disciplina': $routeParams.Id_disciplina , 'pagina': 0 , 'quantidade': 1000};
-        var ctrl = this;
-        this.detalhar = function()
-        {   
-            $scope.ementa = false;
-            ApiOfertaDetalhar.Detalhar(this.formulario,function(resultado) {
-                ctrl.oferta = resultado.corpo;
-                console.log(ctrl.oferta.turmas);
-                for(var i=0, horario = null; i < ctrl.oferta.turmas.length; i++){
-                    console.log(ctrl.oferta.turmas[i].horarios);
-                    for(var j=0; j < ctrl.oferta.turmas[i].horarios[j]; j++) {
-                        console.log(ctrl.oferta.turmas[i].horarios[j].inicio);
-                        console.log(ctrl.oferta.turmas[i].horarios[j].fim);
-                    }
-                }
-            }, function(erro) {
-                ctrl.error = error.data.mensagem;
-                console.log(error.data.mensagem);
-            });
-        }
-        this.detalhar();
-        
-        $scope.MostraEmenta = function() {
-            $scope.ementa = !$scope.ementa;
-        }
+    controller: ['ApiMatricular','$http','$location', 'MatWebGlobals',function(ApiMatricular,$http,$location,MatWebGlobals) {
+      var ctrl = this;
+      this.formulario = {'nome':'','sigla':'','latitude':'','longitude':'','id_campus': 1 };
+      this.cadastrar = function()
+      {
+          ApiMatricular.Cadastrar(ctrl.formulario,function(data){
+            ctrl.mensagem = "Prédio cadastrado com sucesso";
+          },function(data){
+            ctrl.mensagem = "ERRO";
+          });
+    };
     }]
-}). 
-component('gerenciarDisciplina', {
-    templateUrl: '/app/Matricula/matricula.template.html',
-    controller: ['ApiOfertaDetalhar','ApiCampus','ApiDepartamentoPCampus','ApiOfertaPDepart','ApiDisciplinaCadastrar','ApiDisciplinaEditar','ApiDisciplinaDeletar', 'MatWebGlobals', '$routeParams', '$scope', function Detalhar(ApiOfertaDetalhar,ApiCampus,ApiDepartamentoPCampus,ApiOfertaPDepart,ApiDisciplinaCadastrar,ApiDisciplinaEditar,ApiDisciplinaDeletar,MatWebGlobals,$routeParams,$scope) {
-        this.formulario = {'id_disciplina': $routeParams.Id_disciplina , 'pagina': 0 , 'quantidade': 1000};
-        var ctrl = this;
-        ctrl.disciplina = [];
-        ctrl.campus = [];
-        ctrl.departamento = [];
-        
-        ctrl.inicializa = function() {
-            $scope.opcaolistar = false;
-            $scope.listando = false;
-            $scope.gravando = false;
-            $scope.selecionado = false;
-            $scope.editando = false;
-            $scope.selecteddisciplinas = false;
-        };
-        
-        ctrl.listarcampus = function() {
-            ApiCampus.Listar({ nome: "", pagina: 0, quantidade: 1000 },function(resultado) {
-            ctrl.campus = resultado.corpo;      
-            }, function(erro) {
-                ctrl.error = error.data.mensagem;
-            });
-        };
-        
-        $scope.OpcaoListar = function(){
-            ctrl.inicializa();
-            $scope.opcaolistar = true;
-            ctrl.listarcampus();
-        };
-        
-        $scope.listdptos = function(id) {
-            $scope.opcaolistar = false;
-            $scope.listando = true;
-            ApiDepartamentoPCampus.Listar({id_campus: id, 'nome': '', 'pagina': 0, 'quantidade': 1000 }, function(resultado) {
-                ctrl.departamentos = resultado.corpo;
-            }, function(erro) {
-                ctrl.error = error.data.mensagem;
-            });
-        };
-        
-        $scope.SelectDpto = function(departamento) {
-            ctrl.departamento = departamento;
-            if ($scope.gravando == true) {
-                $scope.listando = false;
-                $scope.selecionado = true;
-            } else if ($scope.editando == true ){
-                $scope.selecteddisciplinas = true;
-                $scope.listando = false;
-            } else {
-                ctrl.inicializa();
-                $scope.selecteddisciplinas = true;
-            };
-            ApiOfertaPDepart.Listar({id_departamento: departamento.id, 'nome': '', 'pagina': 0, 'quantidade': 1000 },function(resultado) {
-                ctrl.disciplinas = resultado.corpo;
-            }, function(error){
-                ctrl.logListar = error.data.mensagem;
-            });
-        };
-
-        $scope.Gravar = function() {
-            ctrl.inicializa();
-            $scope.gravando = true;
-            $scope.opcaolistar = true;
-            ctrl.listarcampus();
-        };
-        
-        $scope.Cadastrar = function() {
-            ApiDisciplinaCadastrar.Cadastrar({ 'id_departamento': ctrl.departamento.id, 'nome': ctrl.disciplina.nome, 'codigo': ctrl.disciplina.codigo, 'creditos': ctrl.disciplina.creditos }, function(resultado) {
-                ctrl.disciplina = [];
-                $scope.logCadastrar = "Disciplina Cadastrada com Sucesso";
-            }, function(erro) {
-                $scope.logCadastrar = error.data.mensagem;
-            });
-        };
-        
-        $scope.Escolher = function() {
-            ctrl.inicializa();
-            $scope.opcaolistar = true;
-            $scope.editando = true;
-            ctrl.listarcampus();
-        }
-        
-        $scope.SelectDisciplina = function(disciplina) {
-            ctrl.disciplina = disciplina;
-            $scope.selecteddisciplinas = false;
-            $scope.selecionado = true;
-        }
-        
-        $scope.AlterDisciplina = function() {
-            ApiDisciplinaEditar.Editar({'id':ctrl.disciplina.id, 'id_departamento':ctrl.departamento.id, 'nome':ctrl.disciplina.nome, 'codigo':ctrl.disciplina.codigo, 'creditos':ctrl.disciplina.creditos}, function(resultado) {
-                $scope.logEditar = "Disciplina Alterada com Sucesso";
-                ctrl.disciplina = [];
-            }, function(erro) {
-                $scope.logEditar = error.data.mensagem;
-            });
-        };
-        
-        $scope.DelDisciplina = function() {
-            ApiDisciplinaDeletar.Deletar({'id':ctrl.disciplina.id}, function(resultado) {
-                $scope.logEditar = "Disciplina Apagada com Sucesso";
-                ctrl.disciplina = [];
-            }, function(erro) {
-                $scope.logEditar = error.data.mensagem;
-            });
-        };
-    }]
-});
+}).
+  
--- frontend/app/Matricula/matricula.services.js
+++ frontend/app/Matricula/matricula.services.js
@@ -1,23 +1,9 @@
 angular.
   module('Matricular').
-  factory('ApiOfertaPDepart', ['$resource',
+  factory('ApiMatricular', ['$resource',
     function($resource) {
-        return $resource('/api/Disciplina/Listar', {}, {
-          Listar: { method: 'POST'},
-        });
-    }                                                                      
-  ]).
-  factory('ApiOfertaDetalhar',['$resource',
-    function($resource) {
-        return $resource('/api/Oferta/Detalhar',{}, {
-            Detalhar: { method: 'POST'},
-        });
-    }                                
-]). 
-    factory('ApiDisciplinaCadastrar',['$resource',
-    function($resource) {
-        return $resource('/api/Disciplina/Cadastrar',{}, {
-            Cadastrar: { method: 'POST'},
-        });
-    }   
-]);
+      return $resource('/api/Matricula/Cadastrar', {}, {
+        Cadastrar: { method: 'POST' },
+      });
+    }
+  ]);

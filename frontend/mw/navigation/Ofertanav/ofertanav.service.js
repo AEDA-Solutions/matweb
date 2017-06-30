a860e20b5cd6a7101ba31c047193d21ab94fa465
FelipeAires <felipeaires2008@gmail.com>
2017-06-30 09:07:50 -0300

    Nav

--- frontend/mw/navigation/Ofertanav/ofertanav.service.js
+++ frontend/mw/navigation/Ofertanav/ofertanav.service.js
@@ -2,7 +2,7 @@ angular.
   module('OfertaNav').
   factory('ApiOfertaNav', ['$resource',
     function($resource) {
-      return $resource('/api/Oferta/Listar', {}, {
+      return $resource('/api/Disciplina/Listar', {}, {
         Listar: { method: 'POST' },
       });
     }

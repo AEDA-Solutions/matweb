8b5f0301405809ebcaa9580fdb3968657cf90dcf
FelipeAires <felipeaires2008@gmail.com>
2017-06-29 23:28:45 -0300

    Navegação

--- frontend/mw/navigation/ModalCampusnav/campusnavmodal.component.js
+++ frontend/mw/navigation/ModalCampusnav/campusnavmodal.component.js
@@ -3,15 +3,10 @@ angular.
   component('modalCampusnav', {
     templateUrl: '/mw/navigation/ModalCampusnav/campusnavmodal.template.html',
     bindings: {
-    dismiss: '&'
     }, 
     controller: [ 'MatWebGlobals',function (MatWebGlobals) {
     var ctrl = this;
     ctrl.inter = "";
-
-    ctrl.cancel = function () {
-      ctrl.dismiss({$value: 'cancel'});
-    };
 
      ctrl.inter = MatWebGlobals.Id ;
      console.log(ctrl.inter)
